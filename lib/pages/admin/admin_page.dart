9cdbd947de7500eaf42b78eed7f7db7f637cf5ec
Sedimat <sedimat@portable04.sedimat.priv>
2025-03-03 10:57:48 +0100

    improved admin's page style and added more colours as well

--- lib/pages/admin/admin_page.dart
+++ lib/pages/admin/admin_page.dart
@@ -27,123 +27,130 @@ class AdminPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return BasePage(
       title: AppLocalizations.of(context)!.superAdminPage,
-      body: _buildDashboard(context),
+      body: _buildModernDashboard(context),
     );
   }
 
-  Widget _buildDashboard(BuildContext context) {
-    return GridView.count(
-      crossAxisCount: 2,
-      padding: EdgeInsets.all(20),
-      mainAxisSpacing: 20,
-      crossAxisSpacing: 20,
-      children: [
-        _buildDashboardItem(
-          context,
-          AppLocalizations.of(context)!.manageUsers,
-          Icons.supervised_user_circle,
-          () {
-            Get.to(() => UserManagementPage());
-          },
-        ),
-       /* _buildDashboardItem(
-          context,
-          AppLocalizations.of(context)!.userApprove,
-          Icons.approval,
-          () {
-            Get.to(() => UserApprovalPage());
-          },
-        ), */
-        _buildDashboardItem(
-          context,
-          AppLocalizations.of(context)!.checkList,
-          Icons.checklist,
-          () {
-            Get.to(() => const CheckList());
-          },
-        ),
-        _buildDashboardItem(
-          context,
-          AppLocalizations.of(context)!.listOfLists,
-          Icons.list_alt,
-          () {
-            Get.to(() => const ListOfListsControlPage());
-          },
-        ),
-        _buildDashboardItem(
-          context,
-          AppLocalizations.of(context)!.company,
-          Icons.home_work,
-          () {
-            Get.to(() => CompanyList());
-          },
-        ),
-        _buildDashboardItem(
-          context,
-          AppLocalizations.of(context)!.camionsList,
-          Icons.fire_truck,
-          () {
-            Get.to(() => CamionList());
-          },
-        ),
-        // _buildDashboardItem(
-        //   context,
-        //   AppLocalizations.of(context)!.camionTypesList,
-        //   Icons.fire_truck,
-        //   () {
-        //     Get.to(() => CamionTypeList());
-        //   },
-        // ),
-        // _buildDashboardItem(
-        //   context,
-        //   AppLocalizations.of(context)!.equipmentList,
-        //   Icons.fire_truck,
-        //   () {
-        //     Get.to(() => EquipmentList());
-        //   },
-        // ),
-        _buildDashboardItem(
-          context,
-          AppLocalizations.of(context)!.pdfListAdmin,
-          Icons.picture_as_pdf,
-          () {
-            Get.to(() => AdminPdfListView());
-          },
-        ),
-      ],
+  Widget _buildModernDashboard(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        int crossAxisCount;
+        double childAspectRatio;
+
+        if (constraints.maxWidth > 900) {
+          crossAxisCount = 3;
+          childAspectRatio = 1.5;
+        } else if (constraints.maxWidth > 600) {
+          crossAxisCount = 2;
+          childAspectRatio = 1.5;
+        } else {
+          crossAxisCount = 1;
+          childAspectRatio = 1.8;
+        }
+
+        return CustomScrollView(
+          slivers: [
+            SliverPadding(
+              padding: EdgeInsets.all(16),
+              sliver: SliverGrid(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: crossAxisCount,
+                  childAspectRatio: childAspectRatio,
+                  crossAxisSpacing: 16,
+                  mainAxisSpacing: 16,
+                ),
+                delegate: SliverChildListDelegate([
+                  _buildDashboardItem(
+                    context,
+                    AppLocalizations.of(context)!.manageUsers,
+                    Icons.people,
+                    Colors.blue,
+                    () => Get.to(() => UserManagementPage()),
+                  ),
+                  _buildDashboardItem(
+                    context,
+                    AppLocalizations.of(context)!.checkList,
+                    Icons.checklist,
+                    Colors.green,
+                    () => Get.to(() => const CheckList()),
+                  ),
+                  _buildDashboardItem(
+                    context,
+                    AppLocalizations.of(context)!.listOfLists,
+                    Icons.list_alt,
+                    Colors.orange,
+                    () => Get.to(() => const ListOfListsControlPage()),
+                  ),
+                  _buildDashboardItem(
+                    context,
+                    AppLocalizations.of(context)!.company,
+                    Icons.business,
+                    Colors.purple,
+                    () => Get.to(() => CompanyList()),
+                  ),
+                  _buildDashboardItem(
+                    context,
+                    AppLocalizations.of(context)!.camionsList,
+                    Icons.local_shipping,
+                    Colors.red,
+                    () => Get.to(() => CamionList()),
+                  ),
+                  _buildDashboardItem(
+                    context,
+                    AppLocalizations.of(context)!.pdfListAdmin,
+                    Icons.picture_as_pdf,
+                    Colors.teal,
+                    () => Get.to(() => AdminPdfListView()),
+                  ),
+                ]),
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 
   Widget _buildDashboardItem(
-    BuildContext context, // Ajouter le BuildContext en paramètre
+    BuildContext context,
     String title,
     IconData icon,
+    Color color,
     VoidCallback onTap,
   ) {
     return Card(
-      elevation: 3,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(15),
-      ),
+      elevation: 4,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
       child: InkWell(
         onTap: onTap,
-        borderRadius: BorderRadius.circular(15),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(
-                icon,
-                size: 50,
-                color: Theme.of(context).primaryColor,
-              ),
-              SizedBox(height: 10),
-              Text(
-                title,
-                style: TextStyle(fontSize: 16),
-                textAlign: TextAlign.center,
-              ),
-            ],
+        borderRadius: BorderRadius.circular(20),
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [color.withOpacity(0.7), color],
+            ),
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(icon, size: 48, color: Colors.white),
+                SizedBox(height: 12),
+                Text(
+                  title,
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
           ),
         ),
       ),
