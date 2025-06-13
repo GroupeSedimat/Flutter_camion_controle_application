94695a89847c87a8432b12569a1dfe0c56474988
Sedimat <sedimat@iMac-FA-Marketing.local>
2025-06-13 16:08:22 +0200

    authorise external links

--- lib/services/database_local/equipments_table.dart
+++ lib/services/database_local/equipments_table.dart
@@ -23,31 +23,33 @@ Future<void> createTableEquipments(Database db) async {
   ''');
 }
 
-Future<void> insertEquipment(dynamic dbOrTxn, Equipment equipment, String firebaseId) async {
-  try{
+Future<void> insertEquipment(
+    dynamic dbOrTxn, Equipment equipment, String firebaseId) async {
+  try {
     await dbOrTxn.insert(
-        tableName,
-        equipmentToMap(equipment, firebaseId: firebaseId),
+        tableName, equipmentToMap(equipment, firebaseId: firebaseId),
         conflictAlgorithm: ConflictAlgorithm.replace);
-  } catch (e){
+  } catch (e) {
     print("Error while inserting data into table Equipments: $e");
   }
 }
 
-Future<void> updateEquipment(dynamic dbOrTxn, Equipment equipment, String firebaseId) async {
-  try{
+Future<void> updateEquipment(
+    dynamic dbOrTxn, Equipment equipment, String firebaseId) async {
+  try {
     await dbOrTxn.update(
-        tableName,
-        equipmentToMap(equipment, firebaseId: firebaseId),
+        tableName, equipmentToMap(equipment, firebaseId: firebaseId),
         where: 'id = ?',
         whereArgs: [firebaseId],
         conflictAlgorithm: ConflictAlgorithm.replace);
-  } catch (e){
-    print("Error while updating data for ${equipment.name} in table Equipments: $e");
+  } catch (e) {
+    print(
+        "Error while updating data for ${equipment.name} in table Equipments: $e");
   }
 }
 
-Future<void> updateEquipmentFirebaseID(Database db, Equipment equipment, String firebaseId) async {
+Future<void> updateEquipmentFirebaseID(
+    Database db, Equipment equipment, String firebaseId) async {
   List<String> whereConditions = [];
   List<dynamic> whereArgs = [];
 
@@ -60,22 +62,23 @@ Future<void> updateEquipmentFirebaseID(Database db, Equipment equipment, String 
     whereArgs.add(equipment.idShop);
   }
 
-  String? whereClause = whereConditions.isNotEmpty ? whereConditions.join(' AND ') : null;
+  String? whereClause =
+      whereConditions.isNotEmpty ? whereConditions.join(' AND ') : null;
 
   try {
     await db.update(
-        tableName,
-        equipmentToMap(equipment, firebaseId: firebaseId),
+        tableName, equipmentToMap(equipment, firebaseId: firebaseId),
         where: whereClause,
         whereArgs: whereArgs,
         conflictAlgorithm: ConflictAlgorithm.replace);
-  } catch (e){
-    print("Error while updating data for ${equipment.name} in table Equipments: $e");
+  } catch (e) {
+    print(
+        "Error while updating data for ${equipment.name} in table Equipments: $e");
   }
 }
 
 Future<void> softDeleteEquipment(Database db, String firebaseId) async {
-  try{
+  try {
     await db.update(
         tableName,
         {
@@ -85,93 +88,96 @@ Future<void> softDeleteEquipment(Database db, String firebaseId) async {
         where: 'id = ?',
         whereArgs: [firebaseId],
         conflictAlgorithm: ConflictAlgorithm.replace);
-  } catch (e){
-    print("Error while deleting data with id $firebaseId in table Equipments: $e");
+  } catch (e) {
+    print(
+        "Error while deleting data with id $firebaseId in table Equipments: $e");
   }
 }
 
 Future<void> restoreEquipment(Database db, String firebaseId) async {
-  try{
-    await db.update(
-        tableName,
-        {
-          'updatedAt': DateTime.now().toIso8601String(),
-          'deletedAt': null
-        },
+  try {
+    await db.update(tableName,
+        {'updatedAt': DateTime.now().toIso8601String(), 'deletedAt': null},
         where: 'id = ?',
         whereArgs: [firebaseId],
         conflictAlgorithm: ConflictAlgorithm.replace);
-  } catch (e){
-    print("Error while restoring data with id $firebaseId in table Equipments: $e");
+  } catch (e) {
+    print(
+        "Error while restoring data with id $firebaseId in table Equipments: $e");
   }
 }
 
-Future<Map<String,Equipment>?> getAllEquipments(Database db, String role) async {
+Future<Map<String, Equipment>?> getAllEquipments(
+    Database db, String role) async {
   Map<String, Equipment> equipments = {};
-  try{
+  try {
     final List<Map<String, dynamic>> maps = await db.query(tableName);
-    if(maps.isEmpty){
+    if (maps.isEmpty) {
       return null;
     }
 
     for (var equipmentItem in maps) {
-      if(equipmentItem["deletedAt"] == null || role == "superadmin"){
-        equipments[equipmentItem["id"] as String] = responseItemToEquipment(equipmentItem);
+      if (equipmentItem["deletedAt"] == null || role == "superadmin") {
+        equipments[equipmentItem["id"] as String] =
+            responseItemToEquipment(equipmentItem);
       }
     }
-
-  } catch (e){
+  } catch (e) {
     print("Error while getting all data from table Equipments: $e");
   }
   return sortedEquipments(equipments: equipments);
 }
 
-Future<Map<String,String>?> getAllEquipmentsNames(Database db, String role) async {
+Future<Map<String, String>?> getAllEquipmentsNames(
+    Database db, String role) async {
   Map<String, String> equipmentsNames = {};
-  try{
+  try {
     final List<Map<String, dynamic>> maps = await db.query(tableName);
-    if(maps.isEmpty){
+    if (maps.isEmpty) {
       return null;
     }
 
     for (var equipmentItem in maps) {
-      if(equipmentItem["deletedAt"] == null || role == "superadmin"){
-        equipmentsNames[equipmentItem["id"] as String] = responseItemToEquipment(equipmentItem).name;
+      if (equipmentItem["deletedAt"] == null || role == "superadmin") {
+        equipmentsNames[equipmentItem["id"] as String] =
+            responseItemToEquipment(equipmentItem).name;
       }
     }
-
-  } catch (e){
+  } catch (e) {
     print("Error while getting all Names from table Equipments: $e");
   }
   return equipmentsNames;
 }
 
-Future<Map<String,Equipment>?> getAllEquipmentsSinceLastUpdate(dynamic dbOrTxn, String lastUpdated, String timeSync) async {
+Future<Map<String, Equipment>?> getAllEquipmentsSinceLastUpdate(
+    dynamic dbOrTxn, String lastUpdated, String timeSync) async {
   Map<String, Equipment> equipments = {};
   try {
-    final List<Map<String, dynamic>> maps = await dbOrTxn.query(
-        tableName,
+    final List<Map<String, dynamic>> maps = await dbOrTxn.query(tableName,
         where: 'updatedAt > ? AND updatedAt < ?',
         whereArgs: [lastUpdated, timeSync]);
-    if(maps.isEmpty){
+    if (maps.isEmpty) {
       return null;
     }
     print("-------- last updated Equipments $lastUpdated");
 
     for (var equipmentItem in maps) {
-      print("-------- equipment ${equipmentItem["id"]} updatedAt ${equipmentItem["updatedAt"]}");
-      equipments[equipmentItem["id"] as String] = responseItemToEquipment(equipmentItem);
+      print(
+          "-------- equipment ${equipmentItem["id"]} updatedAt ${equipmentItem["updatedAt"]}");
+      equipments[equipmentItem["id"] as String] =
+          responseItemToEquipment(equipmentItem);
     }
 
     return sortedEquipments(equipments: equipments);
-
-  } catch (e){
-    print("Error while getting all data from table Equipments since last actualisation: $e");
+  } catch (e) {
+    print(
+        "Error while getting all data from table Equipments since last actualisation: $e");
   }
   return sortedEquipments(equipments: equipments);
 }
 
-Future<void> insertMultipleEquipments(dynamic dbOrTxn, Map<String, Equipment> equipments) async {
+Future<void> insertMultipleEquipments(
+    dynamic dbOrTxn, Map<String, Equipment> equipments) async {
   try {
     var batch = dbOrTxn.batch();
 
@@ -189,32 +195,33 @@ Future<void> insertMultipleEquipments(dynamic dbOrTxn, Map<String, Equipment> eq
   }
 }
 
-Future<Equipment?> getOneEquipmentWithID(dynamic dbOrTxn, String equipmentID) async {
-  try{
+Future<Equipment?> getOneEquipmentWithID(
+    dynamic dbOrTxn, String equipmentID) async {
+  try {
     final List<Map<String, dynamic>> maps = await dbOrTxn.query(
       tableName,
       where: 'id = ?',
       whereArgs: [equipmentID],
     );
-    if(maps.isEmpty){
+    if (maps.isEmpty) {
       return null;
     }
 
     return responseItemToEquipment(maps.first);
-
-  } catch (e){
-    print("Error while getting data of Equipment with id $equipmentID from table Equipments: $e");
+  } catch (e) {
+    print(
+        "Error while getting data of Equipment with id $equipmentID from table Equipments: $e");
     return null;
   }
 }
 
 Map<String, dynamic> equipmentToMap(Equipment equipment, {String? firebaseId}) {
   String? available;
-  if(equipment.available == null){
+  if (equipment.available == null) {
     available = null;
-  }else if(equipment.available == false) {
+  } else if (equipment.available == false) {
     available = "false";
-  }else{
+  } else {
     available = "true";
   }
   return {
@@ -222,7 +229,7 @@ Map<String, dynamic> equipmentToMap(Equipment equipment, {String? firebaseId}) {
     'idShop': equipment.idShop,
     'name': equipment.name,
     'description': equipment.description,
-    'photo': (equipment.photo != null && equipment.photo != [] )
+    'photo': (equipment.photo != null && equipment.photo != [])
         ? jsonEncode(equipment.photo!.map((e) => e).toList())
         : null,
     'quantity': equipment.quantity,
@@ -233,24 +240,24 @@ Map<String, dynamic> equipmentToMap(Equipment equipment, {String? firebaseId}) {
   };
 }
 
-Equipment responseItemToEquipment(var equipmentItem){
+Equipment responseItemToEquipment(var equipmentItem) {
   bool available = false;
-  if(equipmentItem["available"] == "true"){
+  if (equipmentItem["available"] == "true") {
     available = true;
   }
   return Equipment(
     idShop: equipmentItem["idShop"] != null
-        ?  equipmentItem['idShop'] as String
+        ? equipmentItem['idShop'] as String
         : null,
     name: equipmentItem["name"] as String,
-    description: equipmentItem["description"]!= null
-        ?  equipmentItem['description'] as String
+    description: equipmentItem["description"] != null
+        ? equipmentItem['description'] as String
         : null,
     photo: equipmentItem["photo"] != null
         ? dataInJsonToList(equipmentItem["photo"] as String)
         : null,
-    quantity: equipmentItem["quantity"]!= null
-        ?  equipmentItem['quantity'] as int
+    quantity: equipmentItem["quantity"] != null
+        ? equipmentItem['quantity'] as int
         : null,
     available: available,
     createdAt: DateTime.parse(equipmentItem["createdAt"] as String),
@@ -261,7 +268,7 @@ Equipment responseItemToEquipment(var equipmentItem){
   );
 }
 
-List<String> dataInJsonToList(String data){
+List<String> dataInJsonToList(String data) {
   final List<dynamic> decodedData = jsonDecode(data);
   return decodedData.map((item) => item as String).toList();
 }
@@ -281,10 +288,10 @@ LinkedHashMap<String, Equipment> sortedEquipments({
         valueA = a.name;
         valueB = b.name;
         break;
-    case 'idShop':
-      valueA = a.idShop;
-      valueB = b.idShop;
-      break;
+      case 'idShop':
+        valueA = a.idShop;
+        valueB = b.idShop;
+        break;
       default:
         valueA = a.name;
         valueB = b.name;
